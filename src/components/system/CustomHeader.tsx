4ecf1c811c2bfec83a0df1a79ae2ca957841b07d
shseok <shseok0674@gmail.com>
2023-08-25 23:05:24 +0900

    Fix: Edit CustomHeader

--- src/components/system/CustomHeader.tsx
+++ src/components/system/CustomHeader.tsx
@@ -3,48 +3,65 @@ import {StyleSheet, Text, View} from 'react-native';
 import {colors} from '../../constants/colors';
 import getDeviceWidth from '../../utils/getDeviceWidth';
 
-const CustomHeader = () => {
+interface Props {
+  hasLeftIcon?: boolean;
+  hasRightIcon?: boolean;
+  headerSize: 'h2' | 'h3' | 'h4';
+  headerText: string;
+}
+
+const headerSizes = {
+  h2: 22,
+  h3: 20,
+  h4: 18,
+};
+
+const CustomHeader = ({
+  hasLeftIcon = false,
+  hasRightIcon = false,
+  headerSize,
+  headerText,
+}: Props) => {
+  const deviceWidth = getDeviceWidth();
+
+  const styles = StyleSheet.create({
+    wrapper: {
+      width: deviceWidth - 71,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    left: {
+      flexDirection: 'row',
+      gap: 8,
+    },
+    icon: {
+      width: 20,
+      height: 20,
+      backgroundColor: colors.gray1,
+    },
+    text: {
+      color: colors.gray5,
+      fontSize: headerSizes[headerSize],
+      fontWeight: '600',
+      lineHeight: 22,
+    },
+    right: {
+      backgroundColor: colors.gray1,
+      width: 20,
+      height: 20,
+    },
+  });
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.left}>
-        <View style={styles.icon} />
-        <Text style={styles.text}>Heading 3</Text>
+        {hasLeftIcon && <View style={styles.icon} />}
+        <Text style={styles.text}>{headerText}</Text>
       </View>
-      <View style={styles.right} />
+      {hasRightIcon && <View style={styles.right} />}
     </View>
   );
 };
-
-const deviceWidth = getDeviceWidth();
-
-const styles = StyleSheet.create({
-  wrapper: {
-    width: deviceWidth - 71,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  left: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-  icon: {
-    width: 20,
-    height: 20,
-    backgroundColor: colors.gray1,
-  },
-  text: {
-    color: colors.gray5,
-    fontSize: 20,
-    fontWeight: '600',
-    lineHeight: 22,
-  },
-  right: {
-    // backgroundColor: colors.gray1,
-    width: 20,
-    height: 20,
-    borderRadius: 50,
-  },
-});
 
 export default CustomHeader;
