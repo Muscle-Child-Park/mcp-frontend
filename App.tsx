f23f090d1035b6dce761f33633469719d796d95c
shseok <shseok0674@gmail.com>
2023-10-24 12:21:36 +0900

    Feat: Apply app icon & splash screens

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import MainStackNavigator from 'src/navigation/MainNavigator';
 // import type {PropsWithChildren} from 'react';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  });
   return <MainStackNavigator />;
 }
 
