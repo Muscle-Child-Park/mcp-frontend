f69bf5fcb5f971077f8052e8c2ac8cf7b07cace5
shseok <shseok0674@gmail.com>
2023-10-26 23:02:38 +0900

    Refactor: Create bottomsheet toggle hook

---
+++ src/assets/hooks/useToggleBottomSheet.tsx
@@ -0,0 +1,62 @@
+import {useEffect, useRef} from 'react';
+import {Dimensions, PanResponder, Animated} from 'react-native';
+
+interface Props {
+  handleCloseModal: () => void;
+  modalVisible: boolean;
+}
+
+export default function useToggleBottomSheet({
+  handleCloseModal,
+  modalVisible,
+}: Props) {
+  const screenHeight = Dimensions.get('screen').height;
+  const panY = useRef(new Animated.Value(screenHeight)).current;
+  const translateY = panY.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [0, 0, 1],
+  });
+
+  const resetBottomSheet = Animated.timing(panY, {
+    toValue: 0,
+    duration: 300,
+    useNativeDriver: true,
+  });
+
+  const closeBottomSheet = Animated.timing(panY, {
+    toValue: screenHeight,
+    duration: 300,
+    useNativeDriver: true,
+  });
+
+  const closeModal = () => {
+    closeBottomSheet.start(() => {
+      handleCloseModal();
+    });
+  };
+
+  const panResponders = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => false,
+      onPanResponderMove: (event, gestureState) => {
+        panY.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        if (gestureState.dy > 0 && gestureState.vy > 1.0) {
+          closeModal();
+        } else {
+          resetBottomSheet.start();
+        }
+      },
+    }),
+  ).current;
+
+  useEffect(() => {
+    if (modalVisible) {
+      resetBottomSheet.start();
+    }
+  }, [modalVisible]);
+
+  return {translateY, panResponders, closeModal};
+}
--- src/components/system/BottomSheet.tsx
+++ src/components/system/BottomSheet.tsx
@@ -1,15 +1,13 @@
-import React, {useEffect, useRef} from 'react';
 import {
   View,
   StyleSheet,
   Modal,
   Animated,
   TouchableWithoutFeedback,
-  Dimensions,
-  PanResponder,
 } from 'react-native';
 import DropdownBox from './DropdownBox';
 import {sortTypes} from 'src/constants/common';
+import useToggleBottomSheet from 'src/assets/hooks/useToggleBottomSheet';
 
 interface Props {
   modalVisible: boolean;
@@ -24,53 +22,12 @@ const BottomSheet = ({
   selected,
   setSelected,
 }: Props) => {
-  const screenHeight = Dimensions.get('screen').height;
-  const panY = useRef(new Animated.Value(screenHeight)).current;
-  const translateY = panY.interpolate({
-    inputRange: [-1, 0, 1],
-    outputRange: [0, 0, 1],
-  });
-
-  const resetBottomSheet = Animated.timing(panY, {
-    toValue: 0,
-    duration: 300,
-    useNativeDriver: true,
-  });
-
-  const closeBottomSheet = Animated.timing(panY, {
-    toValue: screenHeight,
-    duration: 300,
-    useNativeDriver: true,
-  });
-
-  const panResponders = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: () => false,
-      onPanResponderMove: (event, gestureState) => {
-        panY.setValue(gestureState.dy);
-      },
-      onPanResponderRelease: (event, gestureState) => {
-        if (gestureState.dy > 0 && gestureState.vy > 1.0) {
-          closeModal();
-        } else {
-          resetBottomSheet.start();
-        }
-      },
-    }),
-  ).current;
-
-  useEffect(() => {
-    if (modalVisible) {
-      resetBottomSheet.start();
-    }
-  }, [modalVisible]);
-
-  const closeModal = () => {
-    closeBottomSheet.start(() => {
+  const {closeModal, translateY, panResponders} = useToggleBottomSheet({
+    handleCloseModal: () => {
       setModalVisible(false);
-    });
-  };
+    },
+    modalVisible,
+  });
   // console.log(translateY);
   return (
     <Modal
