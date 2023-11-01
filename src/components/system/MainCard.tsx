998253d35e83833f2bdc02c573cb0705037e8373
shseok <shseok0674@gmail.com>
2023-11-01 19:10:17 +0900

    Style: Edit MainCard style

--- src/components/system/MainCard.tsx
+++ src/components/system/MainCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Dimensions, ViewStyle} from 'react-native';
+import {StyleSheet, View, ViewStyle} from 'react-native';
 
 interface Props {
   paddingTop?: number;
@@ -12,19 +12,20 @@ const MainCard = ({children, style}: Props) => {
   return <View style={[styles.card, style]}>{children}</View>;
 };
 
-const deviceWidth = Math.round(Dimensions.get('window').width);
+// const deviceWidth = Math.round(Dimensions.get('window').width);
 const styles = StyleSheet.create({
   card: {
     borderRadius: 8,
     backgroundColor: 'white',
-    width: deviceWidth - 40,
+    // width: deviceWidth - 40,
     // marginHorizontal: 20,
+    width: '100%',
     paddingVertical: 24,
     paddingHorizontal: 16,
     flexDirection: 'column',
     alignItems: 'flex-start',
     gap: 12,
-    elevation: 1,
+    elevation: 2,
   },
 });
 
